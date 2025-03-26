a78e8c23439a7cf55779f769d5dc538a25f27814
SarahHanks04 <sarahhanks04@gmail.com>
2025-03-26 23:34:31 +0100

    Add Navigation component and update RootLayout to include it

---
+++ src/app/components/navigation.jsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+    const pathName = usePathname();
+  return (
+    <nav>
+      <Link href="/" className={pathName === "/" ? "font-bold mr-4" : "text-blue-400 mr-4"}>Home</Link>
+      <Link href="/about" className={pathName === "/about" ? "font-bold mr-4" : "text-blue-400 mr-4"}>About</Link>
+      <Link href="/products/1" className={pathName.startsWith("/products/1") ? "font-bold mr-4" : "text-blue-400 mr-4"}>Products</Link>
+    </nav>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigation from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <header className="bg-slate-400 text-white p-4 text-center">
+      {/* <header className="bg-slate-400 text-white p-4 text-center">
         Welcome To Nextjs 15 Crash Course
-      </header>
+      </header> */}
+      <Navigation />
         {children}
         <footer className="bg-slate-400 text-white p-4 text-center">
           Tutorial by Sarah Hanks

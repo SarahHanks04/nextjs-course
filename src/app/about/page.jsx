3af3c5a870bf2b158023d01e18a5540c167e9a55
SarahHanks04 <sarahhanks04@gmail.com>
2025-03-26 23:41:18 +0100

    Add navigation button to About page for easy access to homepage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,17 @@
-export default function  About () {
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function About() {
+  const router = useRouter();
   return (
-    <h1>About Page</h1>
-  )
+    <div>
+      <h1>About Page</h1>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-blue-400 text-white p-2 rounded-md"
+      >
+        Go Home
+      </button>
+    </div>
+  );
 }

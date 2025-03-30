00b64e9e717ef416692c52c44530be0ab18bcf4a
SarahHanks04 <sarahhanks04@gmail.com>
2025-03-30 20:31:21 +0100

    Add user authentication check to Counter component

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,9 +1,17 @@
 "use client";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useState } from "react";
 
 export const Counter = () => {
+  //   const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const { isLoaded, isSignedIn, user } = useUser();
+
   const [count, setCount] = useState(0);
   console.log("counter component");
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
   return (
     <div>
       <h1>Counter</h1>
